800a669fd9644a6799b54419e096a2b96c0fccda
Emilia <jacaszek.emilia@gmail.com>
2020-02-27 13:11:49 +0100

    style 404 page

--- src/containers/WeatherSearch/NotFound.js
+++ src/containers/WeatherSearch/NotFound.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import './NotFound.css';
 
 
 class NotFound extends Component {
     render() {
 
         return(
-
-            <h3>404 - Not found</h3>
-
+            <div className="text-container">
+            <h3>404 Not found</h3>
+            <p>We couldn't find that page</p>
+            </div>
             )
     }
 }
